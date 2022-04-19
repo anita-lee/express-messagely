ec0f16875e4ca6f0d50b0d120dfb02a681cdcd29
Anita Lee <anita.lee@gmail.com>
2022-04-19 15:15:12 -0400

    added message routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const User = require("../models/user");
-
+const Message = require("../models/message");
+const { ensureCorrectUser } = require("../middleware/auth");
 const Router = require("express").Router;
 const router = new Router();
 
@@ -17,6 +18,11 @@ const router = new Router();
  * Makes sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get("/:id", ensureCorrectUser, async function (req,res){
+  const id = req.params.id;
+  const message = await Message.get(id);
+  return res.json({message});
+})
 
 /** POST / - post message.
  *
@@ -24,6 +30,11 @@ const router = new Router();
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post("/", ensureCorrectUser, async function(req,res){
+  const { to_username, body } = req.body;
+  const message = await Message.create({from_username: res.locals.user, to_username, body});
+  return res.json ({message});
+})
 
 /** POST/:id/read - mark message as read:
  *
@@ -32,5 +43,10 @@ const router = new Router();
  * Makes sure that the only the intended recipient can mark as read.
  *
  **/
+router.post("/:id/read", ensureCorrectUser, async function(req,res){
+  const id = req.params.id;
+  const readMessage = await Message.markRead(id);
+  return res.json ({message: readMessage})
+} )
 
 module.exports = router;
